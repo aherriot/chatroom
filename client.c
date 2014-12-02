0e8d4a93992aa0b35d0622d21ea6f5b666b31bd5
Thomas Cort <linuxgeek@gmail.com>
2014-12-02 16:18:21 -0500

    client.c: exit() after printing usage information.

--- client.c
+++ client.c
@@ -61,8 +61,8 @@ int main(int argc , char *argv[])
 
     if (argc != 3) {
 
-      char errorStr[100];
-      printf(errorStr,"Usage: %s <IP> <port>\n", argv[0]);
+      fprintf(stderr,"Usage: %s <IP> <port>\n", argv[0]);
+      exit(1);
     }
 
     get_username(username);
