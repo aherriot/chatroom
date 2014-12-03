bea06ab60d14a4c6b0be81fb5baab70cb15c89cf
Andrew Herriot <agherriot@gmail.com>
2014-12-02 20:04:01 -0800

    Refactoring client by encapsulating code and making it more clear

--- client.c
+++ client.c
@@ -10,6 +10,7 @@
 
 #include "chatroom_utils.h"
 
+// get a username from the user.
 void get_username(char *username)
 {
   while(true)
@@ -35,146 +36,149 @@ void get_username(char *username)
   }
 }
 
-void set_username(int sock, char *username)
+//send local username to the server.
+void set_username(connection_info *connection)
 {
 
   message username_message;
   username_message.type = SET_USERNAME;
-  strncpy(username_message.username, username, 20);
+  strncpy(username_message.username, connection->username, 20);
 
-  if(send(sock, (void*)&username_message, sizeof(username_message), 0) < 0)
+  if(send(connection->socket, (void*)&username_message, sizeof(username_message), 0) < 0)
   {
     perror("Send failed");
     exit(1);
   }
 }
 
-int main(int argc , char *argv[])
+//initialize connection to the server.
+void connect_to_server(connection_info *connection, char *address, char *port)
 {
-    int sock;
-    struct sockaddr_in server;
-    char server_reply[2000];
-    fd_set file_descriptors;
-    char username[22];
 
-    char stdin_buf[1024];
-    setvbuf(stdin, stdin_buf, _IOFBF, 1024);
+  get_username(connection->username);
+
+  //Create socket
+  if ((connection->socket = socket(AF_INET, SOCK_STREAM , IPPROTO_TCP)) < 0)
+  {
+      perror("Could not create socket");
+  }
+  puts("Socket created\n");
+
+  connection->address.sin_addr.s_addr = inet_addr(address);
+  connection->address.sin_family = AF_INET;
+  connection->address.sin_port = htons(atoi(port));
+
+  //Connect to remote server
+  if (connect(connection->socket , (struct sockaddr *)&connection->address , sizeof(connection->address)) < 0)
+  {
+      perror("Connect failed.");
+      exit(1);
+  }
+
+  set_username(connection);
+
+  puts("Connected\n");
+}
+
+
+void handle_user_input(connection_info *connection)
+{
+  message public_message;
+  public_message.type = PUBLIC_MESSAGE;
+  strncpy(public_message.username, connection->username, 21);
+
+  fgets(public_message.data, 256, stdin);
+
+  //if there is no input, don't send it.
+  if(strlen(public_message.data) == 0) {
+    return;
+  }
+
+  trim_newline(public_message.data);
+
+  //Send some data
+  if(send(connection->socket, &public_message, sizeof(message), 0) < 0)
+  {
+      perror("Send failed");
+      exit(1);
+  }
+}
+
+void handle_server_message(connection_info *connection)
+{
+  message received_message;
+
+  //Receive a reply from the server
+  ssize_t recv_val = recv(connection->socket, &received_message, sizeof(message), 0);
+  if(recv_val < 0)
+  {
+      perror("recv failed");
+      exit(1);
+
+  }
+  else if(recv_val == 0)
+  {
+    close(connection->socket);
+    puts("Server disconnected.");
+    exit(1);
+  }
+
+  switch(received_message.type)
+  {
+
+  case SET_USERNAME:
+    break;
+
+  case PUBLIC_MESSAGE:
+    printf("%s: %s", received_message.username, received_message.data);
+    break;
+
+  case TOO_FULL:
+    printf("Server chatroom is too full to accept new clients.\n");
+    exit(0);
+
+  default:
+    fprintf(stderr, "Unknown message type received.\n");
+    break;
+  }
+}
+
+int main(int argc, char *argv[])
+{
+    connection_info connection;
+    fd_set file_descriptors;
 
     if (argc != 3) {
-
       fprintf(stderr,"Usage: %s <IP> <port>\n", argv[0]);
       exit(1);
     }
 
-    get_username(username);
-
-    //Create socket
-    if ((sock = socket(AF_INET, SOCK_STREAM , IPPROTO_TCP)) < 0)
-    {
-        perror("Could not create socket");
-    }
-    puts("Socket created\n");
-
-    server.sin_addr.s_addr = inet_addr(argv[1]);
-    server.sin_family = AF_INET;
-    server.sin_port = htons( atoi(argv[2]) );
-
-    //Connect to remote server
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        perror("Connect failed.");
-        return 1;
-    }
-
-    puts("Connected\n");
-    set_username(sock, username);
+    connect_to_server(&connection, argv[1], argv[2]);
 
     //keep communicating with server
     while(true)
     {
         FD_ZERO(&file_descriptors);
         FD_SET(STDIN_FILENO, &file_descriptors);
-        FD_SET(sock, &file_descriptors);
+        FD_SET(connection.socket, &file_descriptors);
 
-        if(select(sock+1, &file_descriptors, NULL, NULL, NULL) < 0)
+        if(select(connection.socket+1, &file_descriptors, NULL, NULL, NULL) < 0)
         {
           perror("Select failed.");
         }
 
         if(FD_ISSET(STDIN_FILENO, &file_descriptors))
         {
-          message public_message;
-          public_message.type = PUBLIC_MESSAGE;
-          strncpy(public_message.username, username, 21);
-
-          fgets(public_message.data, 256, stdin);
-
-          //if there is no input, don't send it.
-          if(strlen(public_message.data) == 0) {
-            continue;
-          }
-
-          trim_newline(public_message.data);
-
-          //Send some data
-          if(send(sock, &public_message, sizeof(message), 0) < 0)
-          {
-              perror("Send failed");
-              exit(1);
-          }
-
-          //printf("me: ");
+          handle_user_input(&connection);
         }
 
-        if(FD_ISSET(sock, &file_descriptors))
+        if(FD_ISSET(connection.socket, &file_descriptors))
         {
-
-          message received_message;
-
-
-          //Receive a reply from the server
-          ssize_t recv_val = recv(sock, &received_message, sizeof(message), 0);
-          if(recv_val < 0)
-          {
-              perror("recv failed");
-              break;
-
-          }
-          else if(recv_val == 0)
-          {
-            close(sock);
-            puts("Server disconnected.");
-            break;
-          }
-
-          switch(received_message.type)
-          {
-
-          case SET_USERNAME:
-            break;
-
-          case PUBLIC_MESSAGE:
-            printf("%s: %s", received_message.username, received_message.data);
-            break;
-
-          case TOO_FULL:
-            printf("Server chatroom is too full to accept new clients.\n");
-            exit(0);
-
-          default:
-            fprintf(stderr, "Unknown message type received.\n");
-            break;
-          }
-
-
-          //puts("Server reply :");
-          puts(server_reply);
-
-
+          handle_server_message(&connection);
         }
     }
 
-    close(sock);
+    close(connection.socket);
     return 0;
 }
 
